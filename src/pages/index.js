b3472f00f2961c198e7c3adfb6db31b72e87ea87
leticiaschneider <leticia.schneider@gmail.com>
2024-08-28 10:02:42 -0300

    fix mobile layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,16 @@ import { StaticImage } from "gatsby-plugin-image"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
-  <main className="bg-gray-100">
+  <main className="bg-gray-100 p-10 sm:p-0">
     <div className="max-w-2xl mx-auto pt-20">
 
-      <div className="flex justify-between pt-5">
+      <div className="flex flex-wrap-reverse sm:flex-nowrap sm:justify-between justify-center pt-5">
 
-        <div>
+        <div className="text-center sm:text-left">
           <p className="animate-fade-down font-special-elite text-lg bg-gradient-to-r from-blue-700 via-blue-400 to-blue-300 bg-clip-text text-transparent">&lt;leticiaschneider/&gt;</p>
           <p className="animate-fade-down animate-delay-[400ms] text-3xl my-2">I’m front-end developer</p>
           <p className="animate-fade-down animate-delay-[600ms]">São Paulo, Brazil · leticia.schneider@gmail.com</p>
-          <div className="flex border rounded-full border-solid border-slate-300 w-fit px-3 py-2 mt-3 animate-fade-right animate-delay-[600ms]">
+          <div className="flex mx-auto sm:mx-0 sm:mt-3 border rounded-full border-solid border-slate-300 w-fit px-3 py-2 mt-5 animate-fade-right animate-delay-[600ms]">
             <a className="px-2" href="https://github.com/leticiaschneider">
               <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 496 512" class="hover:fill-slate-600" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg">
                 <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
@@ -40,7 +40,7 @@ const IndexPage = () => (
           </div>
         </div>
 
-        <div>
+        <div className="mb-5 sm:mb-0">
           <StaticImage
             src="../images/my-photo.png"
             loading="eager"
